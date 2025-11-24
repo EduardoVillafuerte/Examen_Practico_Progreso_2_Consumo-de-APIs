33dfd97946b83971751f00d9bf50577d882d60d0
(Estudiante) Edison Eduardo Villafuerte Chinga <edison.villafuerte@udla.edu.ec>
2025-11-24 07:50:50 -0500

    Index Creado

--- Examen_Práctico_Progreso_2_Consumo de APIs/Controllers/PokemonController.cs
+++ Examen_Práctico_Progreso_2_Consumo de APIs/Controllers/PokemonController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +12,14 @@ namespace Examen_Práctico_Progreso_2_Consumo_de_APIs.Controllers
 {
     public class PokemonController : Controller
     {
-        // GET: Pokemon
-        public ActionResult Index()
+        private readonly HttpClient _httpClient = new HttpClient();
+        
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var response = await _httpClient.GetStringAsync("https://pokeapi.co/api/v2/pokemon");
+            JObject json = JObject.Parse(response);
+            var results = json["results"];
+            return View(results);
         }
     }
 }
