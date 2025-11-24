3207497076b94e70a3708f5634879fc3cac33912
(Estudiante) Edison Eduardo Villafuerte Chinga <edison.villafuerte@udla.edu.ec>
2025-11-24 07:34:49 -0500

    Creacion de pokemon controller y vista

---
+++ Examen_Práctico_Progreso_2_Consumo de APIs/Controllers/PokemonController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen_Práctico_Progreso_2_Consumo_de_APIs.Controllers
+{
+    public class PokemonController : Controller
+    {
+        // GET: Pokemon
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
