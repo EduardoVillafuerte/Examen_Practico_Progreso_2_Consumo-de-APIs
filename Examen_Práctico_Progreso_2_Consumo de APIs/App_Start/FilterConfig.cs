9384e4d7b5d38df09dbeba9c24bac210026f2ec8
(Estudiante) Edison Eduardo Villafuerte Chinga <edison.villafuerte@udla.edu.ec>
2025-11-24 07:31:00 -0500

    Add project files.

---
+++ Examen_Práctico_Progreso_2_Consumo de APIs/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Examen_Práctico_Progreso_2_Consumo_de_APIs
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Examen_Práctico_Progreso_2_Consumo de APIs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Examen_Práctico_Progreso_2_Consumo_de_APIs.MvcApplication" Language="C#" %>
